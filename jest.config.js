6cd61b1fc438756bbce8b200ad42193776b827b8
Rysaev Damir <freepad@ya.ru>
2024-10-16 21:45:58 +0300

    add test to ci

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ const config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -107,7 +107,7 @@ const config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [['github-actions', { silent: false }], 'summary']
 
   // Automatically reset mock state before every test
   // resetMocks: false,
