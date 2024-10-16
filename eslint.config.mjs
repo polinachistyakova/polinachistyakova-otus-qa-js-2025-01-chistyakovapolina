292774c9c46a188f93963f05e03236689f73587d
Rysaev Damir <freepad@ya.ru>
2024-10-16 16:25:12 +0300

    eslint config for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import jest from 'eslint-plugin-jest'
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
+  // DOC: https://www.npmjs.com/package/eslint-plugin-jest
+  {
+    files: ['test/**'],
+    ...jest.configs['flat/recommended']
+  }
 ]
